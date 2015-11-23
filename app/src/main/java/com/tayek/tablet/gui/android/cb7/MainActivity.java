5da3be9a849ca07deb8f7c50ecb4c090b6dd03dc
Ray Tayek <ray@tayek.com>
2015-11-22 21:48:33 -0800

    new gui seems to wiggle

--- app/src/main/java/com/tayek/tablet/gui/android/cb7/MainActivity.java
+++ app/src/main/java/com/tayek/tablet/gui/android/cb7/MainActivity.java
@@ -1,13 +1,11 @@
 package com.tayek.tablet.gui.android.cb7;
 import android.app.*;
 import android.content.*;
-import android.content.pm.*;
 import android.graphics.*;
 import android.media.*;
 import android.net.*;
 import android.os.*;
 import android.provider.Settings.*;
-import android.util.*;
 import android.view.*;
 import android.view.View;
 import android.widget.*;
@@ -19,7 +17,6 @@ import com.tayek.tablet.io.*;
 
 import java.lang.*;
 import java.lang.System;
-import java.net.*;
 import java.util.*;
 import java.util.logging.*;
 //https://plus.google.com/103583939320326217147/posts/BQ5iYJEaaEH driver for usb
@@ -30,44 +27,116 @@ public class MainActivity extends Activity implements Observer, View.OnClickList
         NetworkInfo activeNetworkInfo=connectivityManager.getActiveNetworkInfo();
         return activeNetworkInfo!=null&&activeNetworkInfo.isConnected();
     }
-    RelativeLayout buildLayout() { // new gui
-        int size=130;
+    class Gui {
+        Gui(final MainActivity activity) {
+            relativeLayout=new RelativeLayout(activity);
+            int size=130;
+            final int rows=2;
+            final int columns=5;
+            int x0=size/4, y0=75;
+            for(int i=0;i<columns;i++) {
+                on[i]|=0xff000000;
+                off[i]|=0xff000000;
+            }
+            for(Integer i:on)
+                System.out.print(Integer.toString(i,16)+" ");
+            System.out.println();
+            for(Integer i:off)
+                System.out.print(Integer.toString(i,16)+" ");
+            System.out.println();
+            System.out.println("cyan: "+Integer.toString(Color.CYAN,16)+" ");
+            RelativeLayout.LayoutParams params=new RelativeLayout.LayoutParams(50,50);
+            for(int i=0;i<rows*columns;i++) {
+                Button button=new Button(activity);
+                button.setId(i);
+                params=new RelativeLayout.LayoutParams(size,size);
+                params.leftMargin=(int)(x0+i%columns*1.2*size);
+                params.topMargin=(int)(y0+i/columns*size*1.2);
+                button.setLayoutParams(params);
+                button.setText(""+(i+1));
+                if(i/columns%2==1)
+                    button.setBackgroundColor(off[i%columns]);
+                button.setOnClickListener(activity);
+                buttons[i]=button;
+                relativeLayout.addView(button);
+            }
+            Button reset=new Button(activity);
+            reset.setId(rows*columns);
+            params=new RelativeLayout.LayoutParams(size,size);
+            params.leftMargin=(int)(x0+(columns+1.2)*size);
+            params.topMargin=y0;
+            reset.setLayoutParams(params);
+            reset.setText("R");
+            buttons[rows*columns]=reset;
+            relativeLayout.addView(reset);
+        }
+        GuiAdapterABC adapter(Tablet tablet) {
+            guiAdapterABC=new GuiAdapterABC(tablet) {
+                @Override
+                public void setButtonText(final int id,final String string) {
+                    new Timer().schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    buttons[id-1].setText(string);
+                                }
+                            });
+                        }
+                    },0);
+                }
+                @Override
+                public void setButtonState(final int id,final boolean state) {
+                    new Timer().schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    int index=id-1;
+                                    if(index==n) { // reset
+                                        System.out.println("reset");
+                                    } else {
+                                        if(index/columns%2==1)
+                                            buttons[index].setBackgroundColor((!state)?on[index%columns]:off[index%columns]);
+                                        else;
+                                    }
+                                }
+                            });
+                        }
+                    },0);
+                }
+            };
+            return guiAdapterABC;
+        }
+        public String buttonsToString() {
+            String s="{";
+            for(int i=0;i<n;i++)
+                s+=buttons[i].isPressed()?'T':"F";
+            s+='}';
+            return s;
+        }
+        final RelativeLayout relativeLayout;
+        final int n=11;
         final int rows=2;
         final int columns=5;
-        final Integer[] colors=new Integer[columns];
+        Button[] buttons=new Button[n];
+        Integer[] on=new Integer[columns];
+        Integer[] off=new Integer[columns];
         {
-            colors[0]=Color.RED;
-            colors[1]=Color.YELLOW;
-            colors[2]=Color.GREEN;
-            colors[3]=Color.BLUE;
-            colors[4]=Color.rgb(255,165,0);
+            on[0]=0xff0000;
+            on[1]=0xffff00;
+            on[2]=0x00ff00;
+            on[3]=0x0000ff;
+            on[4]=0xffa500;
+            off[0]=0x7e3517;
+            off[1]=0xaf9b60;
+            off[2]=0x254117;
+            off[3]=0x0000A0;
+            off[4]=0xf88017;
         }
-        int x0=size/4,y0=75;
-        RelativeLayout relativeLayout=new RelativeLayout(this);
-        RelativeLayout.LayoutParams params=new RelativeLayout.LayoutParams(50,50);
-        for(int i=0;i<rows*columns;i++) {
-            Button button=new Button(this);
-            button.setId(i+1);
-            params=new RelativeLayout.LayoutParams(size,size);
-            params.leftMargin=(int)(x0+i%columns*1.2*size);
-            params.topMargin=(int)(y0+i/columns*size*1.2);
-            button.setLayoutParams(params);
-            button.setText(""+i);
-            if(i/columns%2==1)
-                button.setBackgroundColor(colors[i%columns]);
-            relativeLayout.addView(button);
-        }
-        Button reset=new Button(this);
-        reset.setId(0);
-        params=new RelativeLayout.LayoutParams(size,size);
-        params.leftMargin=(int)(x0+(columns+1.2)*size);
-        params.topMargin=y0;
-        reset.setLayoutParams(params);
-        reset.setText("R");
-        relativeLayout.addView(reset);
-        return relativeLayout;
     }
-
     Integer id(Sound sound) {
         switch(sound) {
             case electronic_chime_kevangc_495939803:
@@ -81,90 +150,14 @@ public class MainActivity extends Activity implements Observer, View.OnClickList
                 return null;
         }
     }
-    void buildGui(final Tablet tablet,final Toaster toaster) {
-        guiAdapterABC=new GuiAdapterABC(tablet) {
-            @Override
-            public void setButtonText(final int id,final String string) {
-                new Timer().schedule(new TimerTask() {
-                    @Override
-                    public void run() {
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                ((Button)idToButton.get(id)).setText(string);
-                            }
-                        });
-                    }
-                },0);
-            }
-            @Override
-            public void setButtonState(final int id,final boolean state) {
-                new Timer().schedule(new TimerTask() {
-                    @Override
-                    public void run() {
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                System.out.println("set "+id+" checked "+state);
-                                int c=color(id,state);
-                                buttons[id-1].setBackgroundColor(c);
-                            }
-                        });
-                    }
-                },0);
-            }
-        };
-        DisplayMetrics m=getResources().getDisplayMetrics();
-        System.out.println(m);
-        double w=m.widthPixels*.9;
-        double h=m.widthPixels*.9/tablet.group.model.buttons;
-        System.out.println(w+" by +"+h);
-        ViewGroup.LayoutParams lp=new ViewGroup.LayoutParams((int)w,(int)h);
-        System.out.println(lp);
-        LinearLayout layout=new LinearLayout(MainActivity.this);
-        // http://stackoverflow.com/a/11469528/51292
-        // http://android-coding.blogspot.in/2011/05/resize-button-programmatically-using.html
-        layout.setOrientation(LinearLayout.VERTICAL);  //Can also be done in xml by android:orientation="vertical"
-        final TextView top=new TextView(MainActivity.this);
-        top.setText("top");
-        layout.addView(top);
-        MainActivity.this.buttons=new Button[tablet.group.model.buttons];
-        for(int i=1;i<=tablet.group.model.buttons;i++) {
-            Button button=new Button(MainActivity.this);
-            buttons[i-1]=button;
-            button.setLayoutParams(new ViewGroup.LayoutParams(lp));
-            button.setText("Button "+i);
-            button.setId(i);
-            button.setBackgroundColor(color(i,false));
-            layout.addView(button);
-            button.setOnClickListener(MainActivity.this);
-            //gui.idToButton.put(i,button); // too early
-        }
-        bottom=new TextView(MainActivity.this);
-        bottom.setText("bottom\n");
-        layout.addView(bottom);
-        MainActivity.this.layout=layout;
-        System.out.println(idToButton);
-        for(int i=1;i<=tablet.group.model.buttons;i++)
-            idToButton.put(i,buttons[i-1]);
-    }
-    void initialize() {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
         Main.log.init();
         Main.log.setLevel(Level.ALL);
-        // move to common init?
-        System.out.println(System.getProperty("os.name"));
-        Properties properties=java.lang.System.getProperties();
-        logger.info(properties.size()+" properties.");
-        for(Map.Entry<Object,Object> entry : properties.entrySet())
-            logger.info(entry.getKey()+"="+entry.getValue());
-        System.out.println(Arrays.asList(Sound.values()));
-        //
         android_id=Secure.getString(getContentResolver(),Secure.ANDROID_ID);
-        logger.info("android id: '"+android_id+"'");
-        if(!isNetworkAvailable())
-            System.out.println("network is not available!");
-        IO.printNetworkInterfaces();
-        ((Audio.Android)Main.audio).setCallback(new Android.Callback<Sound>() {
+        ((Android)Main.audio).setCallback(new Android.Callback<Sound>() {
             @Override
             public void call(Sound sound) {
                 System.out.println("playing sound.");
@@ -176,59 +169,29 @@ public class MainActivity extends Activity implements Observer, View.OnClickList
         Integer tabletId=Group.tabletIdFromAndroidId(android_id);
         logger.info("tablet id: "+tabletId);
         System.out.println("tablet id: "+tabletId);
-
         if(tabletId==null)
             tabletId=100;
-
         Map<Integer,Group.Info> map=Group.groups.get("g0");
         Group group=new Group(1,map);
-        String host=group.info(tabletId).host;
-        System.out.println("host="+host);
         tablet=new Tablet(group,tabletId);
-        System.out.println("tablet: "+tablet);
         Toaster toaster=new Toaster() {
             @Override
             public void toast(String string) {
                 Toast.makeText(MainActivity.this,string,Toast.LENGTH_SHORT).show();
             }
         };
-        buildGui(tablet,toaster);
         Main.toaster=new Toaster() {
             @Override
             public void toast(final String string) {
-                strings.add(string);
-                if(strings.size()>n)
-                    strings.remove(0);
-                bottom.post(new Runnable() {
-                    public void run() {
-                        String lines="";
-                        for(String string : strings)
-                            lines+=string+"\n";
-                        bottom.setText(lines);
-                    }
-                });
+                System.out.println(string);
             }
-            int n=5;
-            final List<String> strings=new LinkedList();
         };
-        setContentView(layout);
-    }
-    public String buttonsToString() {
-        String s="{";
-        for(int i=0;i<tablet.group.model.buttons;i++)
-            s+=buttons[i].isPressed()?'T':"F";
-        s+='}';
-        return s;
-    }
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        initialize();
+        gui=new Gui(this);
+        guiAdapterABC=gui.adapter(tablet);
+        setContentView(gui.relativeLayout);
         tablet.startListening();
         tablet.group.model.addObserver(this);
         Main.sound=false;
-        setContentView(layout);
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -236,7 +199,6 @@ public class MainActivity extends Activity implements Observer, View.OnClickList
         super.onCreateOptionsMenu(menu);
         for(Tablet.MenuItem menuItem : Tablet.MenuItem.values())
             menu.add(Menu.NONE,menuItem.ordinal(),Menu.NONE,menuItem.name());
-        //getMenuInflater().inflate(R.menu.menu_main,menu);
         return true;
     }
     @Override
@@ -250,13 +212,11 @@ public class MainActivity extends Activity implements Observer, View.OnClickList
     }
     @Override
     protected void onDestroy() {
-        if(tablet!=null) tablet.stopListening();
-        else System.out.println("tablet is null in on destroy!");
+        if(tablet!=null)
+            tablet.stopListening();
+        else
+            System.out.println("tablet is null in on destroy!");
         super.onDestroy();
-    }
-    int color(int id,boolean state) {
-        bg[0]=fg[0]=(float)((id-1)*360./tablet.group.model.buttons);
-        return state?Color.HSVToColor(fg):Color.HSVToColor(bg);
     }
     @Override
     public void onClick(final View v) {
@@ -268,8 +228,8 @@ public class MainActivity extends Activity implements Observer, View.OnClickList
             tablet.group.model.setState(id,!state);
             Message message=new Message(Message.Type.normal,tablet.group.groupId,tablet.tabletId(),id,tablet.group.model.toCharacters());
             tablet.send(message,0);
-            System.out.println("on click: "+id+" "+tablet.group.model+" "+buttonsToString());
-            Main.toaster.toast(buttonsToString());
+            System.out.println("on click: "+id+" "+tablet.group.model+" "+gui.buttonsToString());
+            Main.toaster.toast(gui.buttonsToString());
         } else
             logger.warning("not a button!");
     }
@@ -280,23 +240,12 @@ public class MainActivity extends Activity implements Observer, View.OnClickList
         else
             System.out.println("no gui for model: "+o);
     }
+    Gui gui;
     GuiAdapterABC guiAdapterABC;
     String android_id;
     Tablet tablet;
-    Button[] buttons;
     MediaPlayer mediaPlayer;
     TextView bottom;
     LinearLayout layout;
-    final Map<Integer,Object> idToButton=new LinkedHashMap<>();
-    static float[] fg=new float[3];
-    {
-        fg[1]=1;
-        fg[2]=1;
-    }
-    static float[] bg=new float[3];
-    {
-        bg[1]=.7f;
-        bg[2]=.6f;
-    }
     final Logger logger=Logger.getLogger(getClass().getName());
 }
